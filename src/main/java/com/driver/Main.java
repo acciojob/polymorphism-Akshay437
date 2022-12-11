438e72a5898d209c3aa4545b5603048df6f83fd3
Akshay437 <116643624+Akshay437@users.noreply.github.com>
2022-12-11 20:46:47 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -17,10 +17,10 @@ public class Main {
     }
     public static void main(String[]args)
     {
-        product p= new product();
-        System.out.println(p.product(1,2));
-        System.out.println(p.product(1,2,3));
-        System.out.println(p.product(1.2,2.4));
+        Product P= new Product();
+        System.out.println(P.product(1,2));
+        System.out.println(P.product(1,2,3));
+        System.out.println(P.product(1.2,2.4));
 
     }
 
